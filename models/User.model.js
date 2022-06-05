3ae542575360d8561fc2d9e8a99b286783f3d8e0
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-06-05 10:04:58 +0100

    adds profiltype to payload

--- models/User.model.js
+++ models/User.model.js
@@ -1,12 +1,12 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     profileType: { type: String, enum: ["client", "artist"], required: true },
+    // artistId: { type: Schema.Types.ObjectId, ref: "Artist" },
     favoriteArtists: [{ type: Schema.Types.ObjectId, ref: "Artist" }],
     favoriteTattoos: [{ type: Schema.Types.ObjectId, ref: "Tattoos" }],
     imageUrl: {
@@ -17,7 +17,6 @@ const userSchema = new Schema(
     requestsMade: [{ type: Schema.Types.ObjectId, ref: "Request" }],
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
